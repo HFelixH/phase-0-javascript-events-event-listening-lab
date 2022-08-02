249bc0743431d15624b155f4c64bf761380038b3
HFelixH <felix-hoang@outlook.com>
2022-08-02 11:43:43 +0930

    commit tree 83fb3d4e03cfac56ba43b49a1c636e15511662c8

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-function addingEventListener() {
+const input = document.getElementById('input');
+
+function addEventListener() {
+    input.addEventListener('click', function() {
+        alert('I was clicked!');
+      });
 }
